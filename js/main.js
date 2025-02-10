248e914fff845199284c607887b20e1036c93b92
EthanTecson <ethantecson@gmail.com>
2025-02-10 11:01:05 -0500

    nav drop down refactoring. Mobile few drop down functionality working

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const menuButton = document.getElementById("menu-button");
+    const menuButton = document.getElementById("menu--button");
+    const navDropdown = document.getElementById("nav--dropdown")
     menuButton.addEventListener('click', () => {
-        menuButton.classList.toggle('active');
+        navDropdown.classList.toggle('active');
     });
 });
